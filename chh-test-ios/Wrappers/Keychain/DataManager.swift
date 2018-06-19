e08e4c4e4803fb8a5b89b112def517d8346cb3ed
André Caçador <andre@uniq.agency>
2018-06-19 22:44:47 +0200

    Implementing keychain wrapper to save in a safe way all auth/user data

--- chh-test-ios/Wrappers/Keychain/DataManager.swift
+++ chh-test-ios/Wrappers/Keychain/DataManager.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import KeychainSwift
+
+enum KeychainKey: String {
+    case authentication = "io.jet8.authentication.key"
+}
+
+class DataManager {
+    
+    let keychain = KeychainSwift()
+    
+    func setSecure(_ value: String, forKey: KeychainKey) {
+        keychain.set(value, forKey: forKey.rawValue)
+    }
+    
+    func getSecure(_ key: KeychainKey) -> String? {
+        return keychain.get(key.rawValue)
+    }
+    
+    func deleteSecure(_ key: KeychainKey) -> Bool {
+        return keychain.delete(key.rawValue)
+    }
+    
+    func deleteAllKeys() -> Bool {
+        return keychain.clear()
+    }
+}
