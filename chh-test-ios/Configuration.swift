bf18404e675ccedee12b3412f184ab719172fe4e
André Caçador <andre@uniq.agency>
2018-06-19 22:47:16 +0200

    Setup targets, conf file and urls

--- chh-test-ios/Configuration.swift
+++ chh-test-ios/Configuration.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class Configuration {
+    
+    static let sharedInstance = Configuration()
+    
+    lazy var configs: NSDictionary! = {
+        let currentConfiguration = Bundle.main.object(forInfoDictionaryKey: "App Configuration")!
+        let path = Bundle.main.path(forResource: "Configuration", ofType: "plist")!
+        
+        return NSDictionary(contentsOfFile: path)!.object(forKey: currentConfiguration) as! NSDictionary
+    }()
+    
+}
+
+extension Configuration {
+    func apiEndpoint() -> String {
+        return configs.object(forKey: "APIEndpoint") as! String
+    }
+}
