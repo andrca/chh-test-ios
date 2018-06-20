7f31a363695e68e1eaa6f96148a2e051f1c9d063
André Caçador <andre@uniq.agency>
2018-06-20 14:41:24 +0200

    Modeling historical

--- chh-test-ios/Models/Historical.swift
+++ chh-test-ios/Models/Historical.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import RealmSwift
+import ObjectMapper
+
+class Historical: Object, Mappable {
+    @objc dynamic var coinId = 0
+    @objc dynamic var priceUsd: Double = 0
+    @objc dynamic var snapshotAt = Date()
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        let transformDate = TransformOf<Date, String>(fromJSON: { (value: String?) -> Date? in
+            return DateFormatter.iso8601.date(from: value!)
+        }, toJSON: { (value: Date?) -> String? in
+            return value!.iso8601
+        })
+        
+        coinId     <- map["id"]
+        priceUsd   <- (map["price_usd"], JSONStringToDoubleTransform())
+        snapshotAt <- (map["snapshot_at"], transformDate)
+    }
+    
+}
+
+class JSONStringToDoubleTransform: TransformType {
+    
+    typealias Object = Double
+    typealias JSON = String
+    
+    init() {}
+    func transformFromJSON(_ value: Any?) -> Double? {
+        if let strValue = value as? String {
+            return Double(strValue)
+        }
+        return value as? Double ?? nil
+    }
+    
+    func transformToJSON(_ value: Double?) -> String? {
+        if let doubleValue = value {
+            return "\(doubleValue)"
+        }
+        return nil
+    }
+}
