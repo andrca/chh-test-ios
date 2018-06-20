98c28fcd5f58a468489cbcd200de51d974d506eb
André Caçador <andre@uniq.agency>
2018-06-20 22:39:49 +0200

    Create more models

--- chh-test-ios/Models/GroupedTrades.swift
+++ chh-test-ios/Models/GroupedTrades.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import RealmSwift
+import ObjectMapper
+
+class GroupedTrades: Object, Mappable {
+    @objc dynamic var coinId = 0
+    @objc dynamic var amount: Float = 0
+    @objc dynamic var priceUsd: Float = 0
+    @objc dynamic var coin: Coin?
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        coinId    <- map["coin_id"]
+        amount    <- (map["amount"], JSONStringToFloatTransform())
+        priceUsd  <- (map["price_usd"], JSONStringToFloatTransform())
+    }
+    
+    override static func primaryKey() -> String? {
+        return "coinId"
+    }
+}
--- chh-test-ios/Models/Trade.swift
+++ chh-test-ios/Models/Trade.swift
@@ -7,3 +7,46 @@
 //
 
 import Foundation
+import RealmSwift
+import ObjectMapper
+
+class Trade: Object, Mappable {
+    @objc dynamic var id = 0
+    @objc dynamic var coinId = 0
+    @objc dynamic var userId = 0
+    @objc dynamic var amount: Float = 0
+    @objc dynamic var priceUsd: Float = 0
+    @objc dynamic var totalUsd: Float = 0
+    @objc dynamic var notes: String? = nil
+    @objc dynamic var tradedAt = Date()
+    @objc dynamic var createdAt = Date()
+    @objc dynamic var updatedAt = Date()
+    
+    required convenience init?(map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        let transformDate = TransformOf<Date, String>(fromJSON: { (value: String?) -> Date? in
+            return DateFormatter.iso8601.date(from: value!)
+        }, toJSON: { (value: Date?) -> String? in
+            return value!.iso8601
+        })
+        
+        id        <- map["id"]
+        coinId    <- map["coin_id"]
+        userId    <- map["user_id"]
+        amount    <- map["amount"]
+        priceUsd  <- (map["price_usd"], JSONStringToFloatTransform())
+        totalUsd  <- (map["total_usd"], JSONStringToFloatTransform())
+        notes     <- map["notes"]
+        tradedAt  <- (map["traded_at"], transformDate)
+        createdAt <- (map["created_at"], transformDate)
+        updatedAt <- (map["updated_at"], transformDate)
+    }
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
